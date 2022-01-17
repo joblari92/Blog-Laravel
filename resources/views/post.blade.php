a48a65f95f0753fe8046f78f8b70bea1dd6e17e8
José Blanco Ríos <joseblanco@infofuturo.com>
2022-01-17 21:35:13 +0100

    Commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.layout')
+
+@section('title' , 'POST')
+
+@section('content')
+    <!-- Contenido -->
+    <section class="container-fluid content py-5">
+        <div class="row justify-content-center">
+            <!-- Post -->
+            <div class="col-12 col-md-7 text-center">
+                <h1>POO con Javascript, feo pero posible</h1>
+                <hr>
+                <img src="images/8.png" alt="Post Javascript" class="img-fluid">
+
+                <p class="text-left mt-3 post-txt">
+                    <span>Autor: YouDevs</span>
+                    <span class="float-right">Publicado: Hace 2 semanas</span>
+                </p>
+                <p class="text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque nemo accusantium libero hic repellat corporis assumenda
+                    debitis adipisci modi expedita inventore vel excepturi,
+                    facere animi accusamus? Voluptatem ab ad harum?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque nemo accusantium libero hic repellat corporis assumenda
+                    debitis adipisci modi expedita inventore vel excepturi,
+                    facere animi accusamus? Voluptatem ab ad harum?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque nemo accusantium libero hic repellat corporis assumenda
+                    debitis adipisci modi expedita inventore vel excepturi,
+                    facere animi accusamus? Voluptatem ab ad harum?
+                </p>
+                <p class="text-left post-txt"><i>Categoría: Desarrollo web</i></p>
+            </div>
+
+            <!-- Entradas recientes -->
+            <div class="col-md-3 offset-md-1">
+                <p>Últimas entradas</p>
+                <div class="row mb-4">
+                    <div class="col-4 p-0">
+                        <a href="#">
+                            <img src="images/3.png" class="img-fluid rounded" width="100" alt="">
+                        </a>
+                    </div>
+                    <div class="col-7 pl-0">
+                        <a href="#" class="link-post">Aprende Python en un dos tres</a>
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-4 p-0">
+                        <a href="#">
+                            <img src="images/5.png" class="img-fluid rounded" width="100" alt="">
+                        </a>
+                    </div>
+                    <div class="col-7 pl-0">
+                        <a href="#" class="link-post">PHP sigue vivito y coleando</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+@endsection
